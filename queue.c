607711a266cd7e3bc9ee8df4eccb03c49975e944
gyes00205 <gyes00205@gmail.com>
2021-02-25 19:44:58 +0800

    Initialize size and tail of queue

--- queue.c
+++ queue.c
@@ -13,7 +13,13 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
+    if (!q) {
+        printf('malloc returned NULL');
+        return NULL;
+    }
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
